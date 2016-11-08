0c8f81e5d8840434f956bef07d703cebea317661
Sk0vgaard <Sk0vgaard@live.dk>
2016-11-08 11:29:53 +0100

    Revert "Fixes"
    This reverts commit 8e042ad31594ea9194675b4dff7f4e961f03da89.

--- Guard.java
+++ Guard.java
@@ -6,7 +6,7 @@ import java.awt.Color;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Guard extends Movement
+public class Guard extends Actor
 {
     /**
      * Act - do whatever the Gaurd wants to do. This method is called whenever
